bfa027faaebc0ca3026d5656a256db70c7864b76
Martin Bouchet <bouchetm@avaya.com>
2020-09-19 17:02:27 -0300

    Implement picture downloading to Gallery
    With this change, tap and holding an image to download it to the gallery
    is posible from the PostDetailsFragment.
    Only JPG, JPEG and PNG images are supported.

--- app/src/main/java/com/tincho5588/reddittopsreader/ui/postdetails/PostDetailsFragment.kt
+++ app/src/main/java/com/tincho5588/reddittopsreader/ui/postdetails/PostDetailsFragment.kt
@@ -1,21 +1,25 @@
 package com.tincho5588.reddittopsreader.ui.postdetails
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.drawToBitmap
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.tincho5588.reddittopsreader.R
 import com.tincho5588.reddittopsreader.data.model.Post
 import com.tincho5588.reddittopsreader.util.Utils
+import com.tincho5588.reddittopsreader.util.Utils.downloadPictureToStorage
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.post_details_fragment.*
-import kotlinx.android.synthetic.main.post_details_fragment.author
-import kotlinx.android.synthetic.main.post_details_fragment.comments_number
-import kotlinx.android.synthetic.main.post_details_fragment.created_time_hours
-import kotlinx.android.synthetic.main.post_details_fragment.subreddit
-import kotlinx.android.synthetic.main.post_details_fragment.title
-import kotlinx.android.synthetic.main.post_details_fragment.upvotes_number
 
-class PostDetailsFragment: Fragment(R.layout.post_details_fragment) {
+@AndroidEntryPoint
+class PostDetailsFragment : Fragment(R.layout.post_details_fragment) {
     companion object {
         const val POST_DETAILS_FRAGMENT_TAG = "POST_DETAILS_FRAGMENT"
         const val POST_ARG_KEY = "POST_ARG"
@@ -41,8 +45,10 @@ class PostDetailsFragment: Fragment(R.layout.post_details_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         subreddit.text = post.subreddit_name_prefixed
-        created_time_hours.text = getString(R.string.created_hours_ago,
-            Utils.calculateCreatedTimeHours(post.created_utc).toString())
+        created_time_hours.text = getString(
+            R.string.created_hours_ago,
+            Utils.calculateCreatedTimeHours(post.created_utc).toString()
+        )
         author.text = post.author
 
         title.text = post.title
@@ -61,5 +67,86 @@ class PostDetailsFragment: Fragment(R.layout.post_details_fragment) {
             .load(post.url)
             .error(R.drawable.reddit_logo)
             .into(preview_image)
+
+        registerForContextMenu(preview_image)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu,
+        v: View,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menu.add(0, v.id, 0, R.string.download_menu_option)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        if (item.title == getString(R.string.download_menu_option)) {
+            when (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )) {
+                PackageManager.PERMISSION_GRANTED -> {
+                    requestImageDownload()
+                }
+                else -> {
+                    requestPermissions(
+                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        0
+                    )
+                }
+            }
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            requestImageDownload()
+        } else {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.storage_permission_not_granted),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun requestImageDownload() {
+        if (isValidExtension()) {
+            downloadPictureToStorage(
+                requireContext(),
+                post.id,
+                preview_image.drawToBitmap()
+            ) { success ->
+                if (success) {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.download_successful_message),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.storage_failure_message),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        } else {
+            Toast.makeText(
+                requireContext(),
+                R.string.invalid_picture_format_message,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun isValidExtension(): Boolean {
+        val extension = post.url.substring(post.url.lastIndexOf("."))
+        return listOf(".jpg", ".jpeg", ".png").contains(extension)
     }
 }
--- app/src/main/java/com/tincho5588/reddittopsreader/util/Utils.kt
+++ app/src/main/java/com/tincho5588/reddittopsreader/util/Utils.kt
@@ -1,7 +1,61 @@
 package com.tincho5588.reddittopsreader.util
 
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import com.tincho5588.reddittopsreader.R
+import java.io.File
+import java.io.IOException
+import java.io.OutputStream
+
 object Utils {
     fun calculateCreatedTimeHours(created_utc_seconds: Long): Int {
         return ((System.currentTimeMillis() / 1000 - created_utc_seconds) / 3600).toInt()
+    }
+
+    fun downloadPictureToStorage(context: Context,
+                                 imageName: String,
+                                 bitmap: Bitmap,
+                                 resultCallback: (result: Boolean) -> Unit) {
+        val contentResolver = context.contentResolver
+        val contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        var stream: OutputStream? = null
+        var uri: Uri? = null
+
+        try {
+            val relativeLocation =
+                Environment.DIRECTORY_PICTURES + File.pathSeparator + context.getString(R.string.app_name)
+
+            val contentValues = ContentValues().apply {
+                put(MediaStore.Images.ImageColumns.DISPLAY_NAME, imageName)
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+
+                // Scoped Storage: On Android P devices images will go to the root of the Pictures folder.
+                // Having a different code path to adopt the legacy method only to achieve storing
+                // the picture in a subfolder is not worth it.
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    put(MediaStore.Images.ImageColumns.RELATIVE_PATH, relativeLocation)
+                }
+            }
+
+            uri = contentResolver.insert(contentUri, contentValues)?: throw IOException("Failed to create new MediaStore record.")
+            stream = contentResolver.openOutputStream(uri)
+
+            if (!bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)) {
+                throw IOException("Failed to save bitmap.")
+            }
+
+            resultCallback(true)
+        } catch (e: IOException) {
+            uri?:contentResolver.delete(uri!!, null, null)
+
+            resultCallback(false)
+        } finally {
+            stream?.close()
+        }
     }
 }
