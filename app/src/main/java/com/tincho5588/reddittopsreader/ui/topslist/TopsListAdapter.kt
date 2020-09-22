de260f846c1307a96d35f368c5900079080e7a80
Martin Bouchet <tincho5588@gmail.com>
2020-09-22 03:17:29 -0300

    Introduce Data Binding for the Post List Items
    The code in the PostListAdapter was messy due to the view setup.
    Now, with Data Bindings it all looks a lot more cleaner.
    Other classes that could potentially benefit of Data Bindings
    are the TopListFragment and the PostDetailsFragments.

--- app/src/main/java/com/tincho5588/reddittopsreader/ui/topslist/TopsListAdapter.kt
+++ app/src/main/java/com/tincho5588/reddittopsreader/ui/topslist/TopsListAdapter.kt
@@ -1,53 +1,37 @@
 package com.tincho5588.reddittopsreader.ui.topslist
 
 import android.view.LayoutInflater
-import android.view.View.INVISIBLE
-import android.view.View.VISIBLE
+import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.cardview.widget.CardView
-import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+import com.tincho5588.reddittopsreader.BR
 import com.tincho5588.reddittopsreader.R
 import com.tincho5588.reddittopsreader.data.model.Post
-import com.tincho5588.reddittopsreader.util.Utils.calculateCreatedTimeHours
 
 class TopsListAdapter(var posts: List<Post>, val itemClickedCallback: (post: Post) -> Unit) :
-    RecyclerView.Adapter<TopsListAdapter.ViewHolder>() {
-    class ViewHolder(cardView: CardView) : RecyclerView.ViewHolder(cardView) {
-        lateinit var cardView: CardView
-        lateinit var title: TextView
-        lateinit var subredit: TextView
-        lateinit var created: TextView
-        lateinit var author: TextView
-        lateinit var comments: TextView
-        lateinit var upvotes: TextView
-        lateinit var newIcon: ImageView
-        lateinit var thumbnail: ImageView
+    RecyclerView.Adapter<TopsListAdapter.PostViewHolder>() {
+    class PostViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(post: Post, onClickListener: View.OnClickListener) {
+            binding.setVariable(BR.postItem, post)
+            binding.setVariable(BR.postItemClickListener, onClickListener)
+            binding.executePendingBindings()
+        }
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): ViewHolder {
-        val cardView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.tops_list_item, parent, false) as CardView
-
-        val holder = ViewHolder(cardView)
-        holder.cardView = cardView
-        holder.title = cardView.findViewById(R.id.title)
-        holder.subredit = cardView.findViewById(R.id.subreddit)
-        holder.created = cardView.findViewById(R.id.created_time_hours)
-        holder.author = cardView.findViewById(R.id.author)
-        holder.comments = cardView.findViewById(R.id.comments_number)
-        holder.upvotes = cardView.findViewById(R.id.upvotes_number)
-        holder.newIcon = cardView.findViewById(R.id.new_icon)
-        holder.thumbnail = cardView.findViewById(R.id.preview_image)
-
-        return holder
+    ): PostViewHolder {
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.tops_list_item,
+            parent,
+            false
+        )
+        return PostViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -58,40 +42,10 @@ class TopsListAdapter(var posts: List<Post>, val itemClickedCallback: (post: Pos
         return posts[position]
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
         val item = getItem(position)
 
-        holder.title.text = item.title
-        holder.subredit.text = item.subreddit_name_prefixed
-        holder.author.text = holder.author.context.getString(
-            R.string.user,
-            item.author
-        )
-
-        holder.comments.text = item.num_comments.toString()
-        holder.upvotes.text = item.ups.toString()
-
-        holder.created.text =
-            holder.created.context.getString(R.string.created_hours_ago,
-                calculateCreatedTimeHours(item.created_utc).toString())
-
-        holder.newIcon.visibility = if (item.seen) INVISIBLE else VISIBLE
-        holder.newIcon.setImageDrawable(
-            ContextCompat.getDrawable(
-                holder.newIcon.context,
-                R.drawable.ic_new_black
-            )
-        )
-
-
-        Glide.with(holder.thumbnail.context)
-            .load(item.thumbnail)
-            .error(R.drawable.ic_image_not_supported_black)
-            .into(holder.thumbnail)
-
-        holder.cardView.setOnClickListener {
-            itemClickedCallback(item)
-        }
+        holder.bind(item, View.OnClickListener { itemClickedCallback(item) })
     }
 
     fun updateDataset(newData: List<Post>) {
---
+++ app/src/main/java/com/tincho5588/reddittopsreader/util/BindingAdapters.kt
@@ -0,0 +1,35 @@
+package com.tincho5588.reddittopsreader.util
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.tincho5588.reddittopsreader.R
+
+@BindingAdapter("app:imageUrl")
+fun imageUrl(view: ImageView, url: String) {
+    Glide.with(view.context)
+        .load(url)
+        .error(R.drawable.ic_image_not_supported_black)
+        .into(view)
+}
+
+@BindingAdapter("app:seen")
+fun seen(view: ImageView, seen: Boolean) {
+    ContextCompat.getDrawable(
+        view.context,
+        R.drawable.ic_new_black
+    )
+    view.visibility = if (seen) View.INVISIBLE else View.VISIBLE
+}
+
+@BindingAdapter("app:createHoursAgo")
+fun createdHoursAgo(view: TextView, createdUtc: Long) {
+    view.text =
+        view.context.getString(
+            R.string.created_hours_ago,
+            Utils.calculateCreatedTimeHours(createdUtc).toString()
+        )
+}
